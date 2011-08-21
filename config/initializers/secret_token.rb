a6f820411e3a06c6f82aa8d6f05710c6bab8d6a4
DanMayer <dan@mayerdan.com>
2011-08-21 13:29:07 -0400

    adding rails3 cause well that is what I do

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nothingcalendar::Application.config.secret_token = 'b813105de841ac21a777cabb9d09e2340a9d98f965b3c32f0f732c11844d6f6ad2a3275dc5581f81788ba011260bbccef3c20d4baeb88d7b64b4ebfe380d349a'
