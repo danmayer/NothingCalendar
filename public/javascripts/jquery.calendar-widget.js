78321d9fb8e828771180566dbc6ecc7c1b5b9b66
DanMayer <dan@mayerdan.com>
2011-09-12 21:17:04 -0400

    improve mobile styling

--- public/javascripts/jquery.calendar-widget.js
+++ public/javascripts/jquery.calendar-widget.js
@@ -67,10 +67,10 @@
             for (j=0;j<42;j++){
 			  
               if ((j<firstDay)){
-                table += ('<td class="date-item" id="'+zeroPad(prev_m+1,2)+'-'+zeroPad((prev_days-firstDay+j+1),2)+'-'+prev_y+'"><span class="day">'+ (prev_days-firstDay+j+1) +'</span></td>');
+                table += ('<td class="date-item othermonth" id="'+zeroPad(prev_m+1,2)+'-'+zeroPad((prev_days-firstDay+j+1),2)+'-'+prev_y+'"><span class="day">'+ (prev_days-firstDay+j+1) +'</span></td>');
 			  } else if ((j>=firstDay+getDaysInMonth(month,year))) {
 				i = i+1;
-				table += ('<td class="date-item" id="'+zeroPad(next_m+1,2)+'-'+zeroPad(i,2)+'-'+next_y+'"><span class="day">'+ i +'</span></td>');			 
+				table += ('<td class="date-item othermonth" id="'+zeroPad(next_m+1,2)+'-'+zeroPad(i,2)+'-'+next_y+'"><span class="day">'+ i +'</span></td>');			 
               }else{
                 table += ('<td class="date-item" id="'+zeroPad(month+1,2)+'-'+zeroPad((j-firstDay+1),2)+'-'+year+'"><span class="day">'+(j-firstDay+1)+'</span></td>');
               }
