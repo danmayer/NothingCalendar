bb0e85b8786daf95379e346f59e94eed9b474c95
DanMayer <dan@mayerdan.com>
2011-09-16 21:10:37 -0400

    progress on syncing

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,12 @@ Nothingcalendar::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => "site#index"
 
+  resources :marks ,:only => [] do
+    collection do
+      post 'sync'
+    end
+  end
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
--- db/migrate/20110916014416_add_marks.rb
+++ db/migrate/20110916014416_add_marks.rb
@@ -1,9 +1,11 @@
 class AddMarks < ActiveRecord::Migration
   def self.up
     add_column :users, :marks_data, :text
+    add_column :users, :last_updated, :timestamp
   end
 
   def self.down
     drop_column :users, :marks_data
+    drop_column :users, :last_updated
   end
 end
