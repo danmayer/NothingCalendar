855e8008881056039047fffe2eac0774ca6e0f5d
DanMayer <dan@mayerdan.com>
2011-11-08 20:34:36 -0500

    added some additional marks tests

--- test/unit/mark_test.rb
+++ test/unit/mark_test.rb
@@ -5,8 +5,24 @@ class UserTest < ActiveSupport::TestCase
   test "creates a valid mark" do
     time = Time.now
     mark = Mark.new( {:last_updated => time,
-                :marks_data => [].to_json})
+                :marks_data => []})
     assert_equal time, mark.last_updated
+  end
+
+  test "creates a valid mark from params" do
+    time = Time.now
+    mark = Mark.from_params([{'key' => 'last_updated', 'val' => time},
+                             {'key' => '11-15-2011', 'val' => true}].to_json)
+    assert_equal time.to_i, Time.parse(mark.last_updated).to_i
+    assert_equal [{"val"=>true, "key"=>"11-15-2011"}], mark.marks_data
+  end
+
+  test "creates returns as_hash" do
+    time = Time.now
+    mark = Mark.new( {:last_updated => time,
+                :marks_data => []})
+    assert_equal time, mark.as_hash[:last_updated]
+    assert_equal [], JSON.parse(mark.as_hash[:marks_data])
   end
 
 end
